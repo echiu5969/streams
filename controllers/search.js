c704bdffe222df79b3c02220cb47f1ce8a3e2459
echiu5969 <echiu5969@bths.edu>
2016-05-28 18:14:53 +0000

    search js

--- controllers/search.js
+++ controllers/search.js
@@ -3,6 +3,14 @@ var Search = {
     
     index: function(req, res){
         res.render('search/index');
+    },
+    search: function(req, res, next) {
+        var data = {
+            items:[0, 1,2 ,3 ,4 ,5],
+            numItems: 6,
+            SearchTerms: ''
+        };
+        res.render('search/index', data);
     }
 };
 
